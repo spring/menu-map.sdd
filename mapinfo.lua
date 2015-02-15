dad422950c0fe9dd5b6ce5f48a8082c9300076bf
abma <spring@abma.de>
2015-02-15 01:18:40 +0100

    add mapinfo.lua

---
+++ mapinfo.lua
@@ -0,0 +1,62 @@
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- mapinfo.lua
+--
+
+local mapinfo = {
+	name        = "Spring Menu - Map",
+	shortname   = "SMM",
+	description = "Spring engine menu",
+	author      = "",
+	version     = "$VERSION",
+	--mapfile   = "", --// location of smf/sm3 file (optional)
+	modtype     = 3, --// 1=primary, 0=hidden, 3=map
+	depend      = {"Map Helper v1"},
+	replace     = {},
+
+	maphardness     = 100,
+	notDeformable   = false,
+	gravity         = 130,
+	tidalStrength   = 0,
+	maxMetal        = 0.02,
+	extractorRadius = 500.0,
+	voidWater       = false,
+	autoShowMetal   = true,
+
+
+	smf = {
+	},
+
+	sound = {
+	},
+
+	resources = {
+	},
+
+	splats = {
+	},
+
+	atmosphere = {
+	},
+
+	grass = {
+	},
+
+	lighting = {
+	},
+	
+	water = {
+	},
+
+	teams = {
+	},
+
+	terrainTypes = {
+	},
+
+}
+
+return mapinfo
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
